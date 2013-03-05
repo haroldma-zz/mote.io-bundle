d7570971fef912dff92ef3f7742a6c0ef2f7e50c
Ian Jennings <ian@meetjennings.com>
2013-03-05 00:53:35 -0500

    lazy loading of assets

--- extension/inject.js
+++ extension/inject.js
@@ -6,4 +6,34 @@ function appendScript(scriptFile) {
    document.documentElement.appendChild(script); // run the script
 }
 
+appendScript('lib/jquery.js');
+appendScript('lib/socket.io.js');
+appendScript('lib/jquery.qrcode.min.js');
 appendScript('moteio.js');
+
+
+// Executing an anonymous script
+function exec(fn) {
+   var script = document.createElement('script');
+   script.setAttribute("type", "application/javascript");
+   script.textContent = '(' + fn + ')();';
+   document.documentElement.appendChild(script); // run the script
+   document.documentElement.removeChild(script); // clean up
+}
+
+script = function() {
+  setInterval(function(){
+    if($) {
+      var j$ = $.noConflict();
+      console.log('have jquery')
+    }
+    if(j$('body').qrcode('test')) {
+      console.log('have qrcode')
+    }
+    if(io){
+      console.log('have socket.io')
+    }
+  }, 2000)
+}
+
+exec(script);
