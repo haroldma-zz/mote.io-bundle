fbf2d94259d370775b285a1c786364ad58bec4e4
Ian Jennings <ian@meetjennings.com>
2013-04-18 21:19:51 -0400

    app update to handle reconnections better

--- app/assets/www/moteio.js
+++ app/assets/www/moteio.js
@@ -328,25 +328,27 @@ var App = function () {
 
     var data = null;
 
+    $.mobile.changePage($('#login'));
+
+    var networkState = navigator.connection.type;
+    var states = {};
+
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    console.log('connection state is ' + states[networkState]);
+
     $("#login-form").submit(function (e) {
 
       e.preventDefault();
 
       $.mobile.changePage($('#loading'));
-
-      /*
-      var networkState = navigator.network.connection.type;
-      var states = {};
-      states[Connection.UNKNOWN]  = 'Unknown connection';
-      states[Connection.ETHERNET] = 'Ethernet connection';
-      states[Connection.WIFI]     = 'WiFi connection';
-      states[Connection.CELL_2G]  = 'Cell 2G connection';
-      states[Connection.CELL_3G]  = 'Cell 3G connection';
-      states[Connection.CELL_4G]  = 'Cell 4G connection';
-      states[Connection.NONE]     = 'No network connection';
-
-      alert('Connection type: ' + states[networkState]);
-      */
 
       var data = $(this).serializeArray();
 
