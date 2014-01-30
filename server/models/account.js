e7ff426360845cf352fa481ceaba0bc3f4a2b8f8
Ian Jennings <ian@meetjennings.com>
2014-01-30 12:04:15 -0500

    better error handling for bad username or pass

--- server/models/account.js
+++ server/models/account.js
@@ -11,6 +11,10 @@ var Account = new Schema({
   channel: String
 });
 
-Account.plugin(passportLocalMongoose, {iterations: 250});
+Account.plugin(passportLocalMongoose, {
+	iterations: 250,
+	incorrectPasswordError: "Bad password. Please try again or reset it at http://mote.io/reset.",
+	incorrectUsernameError: "Can not find account with that email address. Visit http://mote.io/start on your computer."
+});
 
 module.exports = mongoose.model('Account', Account);
